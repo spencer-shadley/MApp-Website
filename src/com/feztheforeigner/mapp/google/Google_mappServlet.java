c6134a612c5a4145674360053c2d24f937080e1d
U-Spencer-MSI\Spencer <spencershadley@utexas.edu>
2015-01-06 23:45:43 -0600

    a few fixes and color changes

---
+++ src/com/feztheforeigner/mapp/google/Google_mappServlet.java
@@ -0,0 +1,13 @@
+package com.feztheforeigner.mapp.google;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Google_mappServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
