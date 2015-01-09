29315293cbafe8fcb876a0885f346ef231d37cb6
U-Spencer-MSI\Spencer <spencershadley@utexas.edu>
2015-01-09 00:47:17 -0600

    actually finished resize of iframes

--- war/js/units.js
+++ war/js/units.js
@@ -1,20 +1,63 @@
+// set default heights and widths for iframes
+var default_width = 960;
+var default_height = 749;
+var default_doc_height = 300;
+var default_ratio = default_width / default_height;
+
 // Load the header
 $(function() { $("#header").load("../header.html"); });
 
-// Adjust iframe width on load and resize
+// Adjust iframe width on load
 $(document).ready(function() {
 	resizeFrames();
 });
+
+//Adjust iframe width on resize
 $(window).resize(function() {
 	resizeFrames();
 });
 
+// update iframe width and height
 function resizeFrames() {
+	
+	var iframe_ratio = default_width / default_height;		// needed to calculate an updated height
 	var win_width = $(window).width();
-	if(win_width < 900) {
-		var iframes = document.getElementsByTagName("iframe");
+	var iframes = document.getElementsByTagName("iframe");
+	
+	// if the window is between 100 and default_width pixels > update
+	if(win_width<default_width && win_width>100) {
+		
+		// for each iframe, calculate and set a new width and height
 		for(var i=0; i<iframes.length; i++) {
-			iframes[i].setAttribute("width", win_width - 50);
+			var updated_width = win_width - 50;
+			var updated_height = updated_width / default_ratio;
+
+			var iframe = iframes[i];
+			var iclass = iframe.className;
+			
+			iframe.setAttribute("width", updated_width);
+			if(iclass==="slidedeck") {
+				iframe.setAttribute("height", updated_height);
+			}
+			else if(iclass==="doc"){
+				iframe.setAttribute("height", default_doc_height);
+			}
+		}
+	}
+	
+	// otherwise set defaults
+	else {
+		for(var i=0; i<iframes.length; i++) {
+			var iframe = iframes[i];
+			var iclass = iframe.className;
+			
+			iframe.setAttribute("width", default_width);
+			if(iclass==="slidedeck") {
+				iframe.setAttribute("height", default_height);
+			}
+			else if(iclass==="doc"){
+				iframe.setAttribute("height", default_doc_height);
+			}
 		}
 	}
 }
