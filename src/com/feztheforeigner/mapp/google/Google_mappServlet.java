716948b5aeada92deba9a0380a37745a2d17dcd4
U-Spencer-MSI\Spencer <spencershadley@utexas.edu>
2015-01-12 00:44:15 -0600

    began implementing new servlet, new url patterns

--- src/com/feztheforeigner/mapp/google/Google_mappServlet.java
+++ src/com/feztheforeigner/mapp/google/Google_mappServlet.java
@@ -8,6 +8,6 @@ public class Google_mappServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.getWriter().println("Hello, Google_mappServlet.java");
 	}
 }
