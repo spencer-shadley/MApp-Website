35ecc4032ff88aab66cf761149492145c0ee4542
U-Spencer-MSI\Spencer <spencershadley@utexas.edu>
2015-01-16 03:33:06 -0600

    added ads, tweaking throghout

--- war/js/unit-list.js
+++ war/js/unit-list.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
 
+	// faster with direct, no script
 	// set all unit descriptions
-	 var descs = document.getElementsByClassName("unit_desc");
+	 /*var descs = document.getElementsByClassName("unit_desc");
 
 	 for(var i=0; i<descs.length/2; i++) {
 		 descs[i].innerHTML = unitTitles[i];
-	 }
+	 }*/
 	 
 	/* var j=0;
 	 for(var i=descs.length/2; i<descs.length; i++) {
